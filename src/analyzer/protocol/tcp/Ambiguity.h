b4695322e3d6e6fd88b548e3358f59429a94e0d7
Zhongjie Wang <wzj401@gmail.com>
2021-05-04 18:43:52 -0700

    update comments

--- src/analyzer/protocol/tcp/Ambiguity.h
+++ src/analyzer/protocol/tcp/Ambiguity.h
@@ -6,14 +6,14 @@ namespace zeek::analyzer::tcp {
 
 enum
 {
-    AMBI_SYNFIN_IN_LISTEN, /* SYN + FIN packet in LISTEN state -- 1 */
-    AMBI_IN_WINDOW_SYN, /* In-window SYN in ESTABLISHED state -- 2 */
-    AMBI_IN_WINDOW_RST, /* In-window RST in ESTABLISHED state -- 3 */
-    AMBI_TOO_OLD_ACK_NUM, /* Too old ACK number in ESTABLISHED state -- 4 */
-    AMBI_NO_ACK, /* Data packets without ACK flag -- 5 */
-    AMBI_RST_RIGHTMOST_SACK, /* RST packets with SEQ = rightmost SACK -- 6*/
-    AMBI_RST_AFTER_FIN, /* RST packets with SEQ = rcv_nxt - 1 in closing states -- 7*/
-    AMBI_DATA_IN_CLOSING_STATES, /* Data packets with old ACK number in closing states -- 8*/
+    AMBI_SYNFIN_IN_LISTEN, /* SYN + FIN packet in LISTEN state */
+    AMBI_IN_WINDOW_SYN, /* In-window SYN in ESTABLISHED state */
+    AMBI_IN_WINDOW_RST, /* In-window RST in ESTABLISHED state */
+    AMBI_TOO_OLD_ACK_NUM, /* Too old ACK number in ESTABLISHED state */
+    AMBI_NO_ACK, /* Data packets without ACK flag */
+    AMBI_RST_RIGHTMOST_SACK, /* RST packets with SEQ = rightmost SACK */
+    AMBI_RST_AFTER_FIN, /* RST packets with SEQ = rcv_nxt - 1 in closing states */
+    AMBI_DATA_IN_CLOSING_STATES, /* Data packets with old ACK number in closing states */
     AMBI_MAX  /* Leave at the end! */
 };
 
